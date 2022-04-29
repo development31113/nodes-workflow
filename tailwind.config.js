fabc9c2dac917aa8a293ddd941cf4dcd301a2341
freezpl <freezpl@gmail.com>
2022-04-29 13:48:47 +0300

    setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '2px': '2px',
+      },
+    },
+  },
+  plugins: [],
+}
