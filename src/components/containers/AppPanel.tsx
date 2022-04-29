a1b284d4f384772555197376c7f3bc59993d6f87
freezpl <freezpl@gmail.com>
2022-04-29 18:01:36 +0300

    fixing app-panel template

--- src/components/containers/AppPanel.tsx
+++ src/components/containers/AppPanel.tsx
@@ -10,8 +10,8 @@ interface IAppPanelProps {
 const AppPanel: React.FC<IAppPanelProps> = ({ title, className, children }) => {
   return (
     <div className={classNames('tw-flex tw-flex-col tw-h-full tw-w-full tw-border-2 tw-border-solid tw-border-slate-400', className)}>
-      <div className="tw-bg-slate-300 tw-font-semibold tw-text-slate-800 tw-text-sm tw-py-2px tw-px-2">{title}</div>
-      <div className="tw-bg-slate-100 tw-p-2">{children}</div>
+      <div className="tw-bg-blue-300 tw-font-semibold tw-text-blue-900 tw-text-sm tw-py-2px tw-px-2">{title}</div>
+      <div className="tw-bg-slate-100 tw-p-2 tw-h-full">{children}</div>
     </div>
   );
 };
